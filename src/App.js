8eda8802f11bb553180d3eccc56a9a181ded0378
fenil29 <fenilkaneria@gmail.com>
2022-05-14 15:31:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import ReactLoading from 'react-loading'
+import Skeleton from 'react-loading-skeleton'
+import { Configuration, OpenAIApi } from 'openai'
+
+import 'react-loading-skeleton/dist/skeleton.css'
+import './App.css'
+
+const configuration = new Configuration({
+  apiKey: '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',
+})
+const openai = new OpenAIApi(configuration)
 
 function App() {
+  const [textPrompt, setTextPrompt] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [responses, setResponses] = useState([])
+  const [supportedEngines, setSupportedEngines] = useState([])
+  const [selectedEngines, setSelectedEngines] = useState('text-curie-001')
+
+  function handleChange(event) {
+    setTextPrompt(event.target.value)
+  }
+  function handleSubmit(event) {
+    setIsLoading(true)
+    event.preventDefault()
+    fetchDataFromOpenAi(textPrompt)
+    setTextPrompt('')
+  }
+
+  async function fetchDataFromOpenAi(textPrompt) {
+    try {
+      const completion = await openai.createCompletion(selectedEngines, {
+        prompt: textPrompt,
+      })
+      console.log(completion.data.choices[0].text)
+      let temp = [...responses]
+      temp.unshift({
+        prompt: textPrompt,
+        response: completion.data.choices[0].text,
+        engine: selectedEngines,
+      })
+      setResponses(temp)
+      saveResponses(temp)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      alert('Something Went Wrong!')
+    }
+  }
+  useEffect(() => {
+    loadResponses()
+    loadSupportedEngines()
+  }, [])
+  function saveResponses(responses) {
+    localStorage.setItem('responses', JSON.stringify(responses))
+  }
+  function loadResponses() {
+    if (localStorage.getItem('responses')) {
+      setResponses(JSON.parse(localStorage.getItem('responses')))
+    }
+  }
+  function clearResponses() {
+    saveResponses([])
+    setResponses([])
+  }
+  function loadResponses() {
+    if (localStorage.getItem('responses')) {
+      setResponses(JSON.parse(localStorage.getItem('responses')))
+    }
+  }
+  async function loadSupportedEngines() {
+    const supportedEngines = await openai.listEngines()
+
+    setSupportedEngines(supportedEngines.data.data)
+    console.log(supportedEngines.data.data)
+  }
+  function handleSelectEngine(e) {
+    let { value } = e.target
+    console.log(value)
+    setSelectedEngines(value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="applicaion-container">
+      <h1>Fun with AI</h1>
+      {supportedEngines.length === 0 ? (
+        <ReactLoading type={'spin'} color={'#004c3f'} height={20} width={20} />
+      ) : (
+        <select name="engine" id="engine" onChange={handleSelectEngine}>
+          {supportedEngines.map((engine) => (
+            <option value={engine.id} selected={engine.id === 'text-curie-001'}>
+              {engine.id}
+            </option>
+          ))}
+        </select>
+      )}
+
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Enter Prompt:</p>
+            <textarea value={textPrompt} onChange={handleChange} />
+            <div className="button-container">
+              <button type="submit" value="Submit">
+                Submit
+              </button>
+            </div>
+          </label>
+        </form>
+      </div>
+      {responses.length > 0 && (
+        <div className="clear-button-container">
+          <button onClick={clearResponses}>Clear</button>
+        </div>
+      )}
+
+      {isLoading && <Skeleton count={1} />}
+      {responses.map((response, index) => (
+        <div key={index} className="response-container">
+          <h2>Prompt:</h2>
+          <p>{response.prompt}</p>
+          <h2>Response:</h2>
+          <p>{response.response}</p>
+          <h2>Engine:</h2>
+          <p>{response.engine}</p>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
